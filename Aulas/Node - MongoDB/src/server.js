e3bf816f651796e71fda66b91e6f480716c40413
Joao Textor <joaotextor@gmail.com>
2022-11-25 11:39:48 -0300

    Installed ejs

---
+++ Aulas/Node - MongoDB/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//define template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//define public files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//enable server to recognize data using 'post' (form)
+app.use(express.urlencoded({ extended: true}))
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Título Teste'
+    })
+})
+
+//404 error
+app.use((req, res) => {
+    res.send('Página não encontrada!')
+})
+
+//initializar server
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
+
