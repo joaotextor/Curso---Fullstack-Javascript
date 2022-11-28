124c2c2de5072ad7ab6c10fe41577e39f2de4bef
Joao Textor <joaotextor@gmail.com>
2022-11-28 17:49:31 -0300

    frontend to consume API. List and add products

--- Aulas/Node - API RESTFul - Frontend/script.js
+++ Aulas/Node - API RESTFul - Frontend/script.js
@@ -1,6 +1,62 @@
-fetch('http://localhost:8080/api/products')
+//CACHE
+const productsList = document.getElementById('products-list')
+
+const form = document.getElementById('form')
+
+
+
+//obtain products list
+
+const API_URL = 'http://localhost:8080/api/products'
+
+const obtainProducts = () => {
+
+    fetch(API_URL)
     .then(response => {
         response.json().then(data => {
-            console.log(data)
+            
+            const productsHtml = data.map(product => `
+            <li>
+            ${product.name} - ${product.brand} - ${product.price}
+            </li>
+            `).join('')
+            
+            productsList.innerHTML = productsHtml
         })
+        
     })
+}
+
+obtainProducts()
+
+// REGISTER PRODUCT
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+
+    const name = document.forms['form'].name.value
+    const brand = document.forms['form'].brand.value
+    const price = document.forms['form'].price.value
+
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            brand,
+            price
+        })
+    }).then(response => {
+            response.json().then(data => {
+                if (data.message === 'success') {
+                    obtainProducts()
+                    form.reset()
+                    alert('Cadastro realizado com sucesso!')
+                } else {
+                    alert('Ops, ocorreu um erro. Tente novamente.')
+                }
+            })
+        })
+}
