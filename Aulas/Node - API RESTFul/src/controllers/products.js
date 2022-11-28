37017b6b2fb09a0f332356c4f62b1fcbf78dd9c0
Joao Textor <joaotextor@gmail.com>
2022-11-28 14:38:08 -0300

    get method for product

--- Aulas/Node - API RESTFul/src/controllers/products.js
+++ Aulas/Node - API RESTFul/src/controllers/products.js
@@ -1,7 +1,9 @@
 const ProductsModel = require('../modules/products')
 
 const get = async (req, res) => {
-    const products = await ProductsModel.find()
+    const { id } = req.params
+    
+    const products = await ProductsModel.find(id ? { _id: id } : null)
     
     res.send(products)
 }
