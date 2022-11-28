5d2a13f2d2ed7c2ccff0f3afb762b28c1dd4ff4c
Joao Textor <joaotextor@gmail.com>
2022-11-28 17:09:57 -0300

    added cors middleware

--- Aulas/Node - API RESTFul/src/server.js
+++ Aulas/Node - API RESTFul/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -7,6 +7,24 @@ const routes = require('./routes/routes')
 const app = express()
 
 db.connect()
+
+const allowedOrigins = [
+    'http://127.0.0.1:5502',
+    'http://127.0.0.1:5501',
+]
+
+//enable cors
+app.use(cors({
+    origin: function(origin, callback) {
+        let allowed = true
+
+        !origin ? allowed = true : null
+
+        !allowedOrigins.includes(origin) ? allowed = false : null
+
+        callback(null, allowed)
+    }
+}))
 
 //enable server to receive json data
 app.use(express.json())
