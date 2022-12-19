197b97250c575a4f6c3fbe48ddb9b626e0149f18
Joao Textor <joaotextor@gmail.com>
2022-12-18 21:05:26 -0300

    add ThemeProvider to index page

--- Aulas/my_app/src/index.js
+++ Aulas/my_app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from '@mui/material/styles'
 
+import { defaultTheme } from './themes/defaultTheme'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
 
