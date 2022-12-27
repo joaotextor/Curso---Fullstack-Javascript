2ae008500aac19a7f07d8cc3c1b9a81aa6362887
Joao Textor <joaotextor@gmail.com>
2022-12-27 10:42:27 -0300

    Manage states on login page

--- Aulas/my_app/src/pages/Login.js
+++ Aulas/my_app/src/pages/Login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { 
     Typography,
     TextField,
@@ -12,26 +13,51 @@ const Wrapper = styled('div')`
 `
 
 export default function Login() {
+
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+
+        setForm({
+            ...form,
+            [name]: value,
+        })
+    }
+
+    const handleFormSubmit = () => {
+        console.log(form)
+    }
+
     return (
         <>
             <Typography variant="h3">Restricted Access</Typography>
 
             <Wrapper>
                 <TextField
-                    label="Insert your email address"
+                    variant="standard"
+                    type="email"
+                    label="Insert your email"
                     name="email"
+                    onChange={handleInputChange}
                 />
             </Wrapper>
 
             <Wrapper>
                 <TextField
+                    variant="standard"
+                    type="password"
                     label="Insert your password"
                     name="password"
+                    onChange={handleInputChange}
                 />
             </Wrapper>
 
             <Wrapper>
-                <Button variant="contained" color="primary">Login</Button>
+                <Button variant="contained" color="primary" onClick={handleFormSubmit}>Login</Button>
             </Wrapper>
         </>
     )
